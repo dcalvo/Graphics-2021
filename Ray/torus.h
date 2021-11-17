97e2684cb41a8cd0beaa4ae41169f3afefb24c7f
David <dcalvo2@jhu.edu>
2021-11-16 20:48:11 -0500

    implement torus

--- Ray/torus.h
+++ Ray/torus.h
@@ -3,12 +3,11 @@
 #include <Util/geometry.h>
 #include <Util/polynomial.h>
 #include "shape.h"
+#include "triangle.h"
 
-namespace Ray
-{
+namespace Ray {
 	/** This class describes a torus, and is represented by its center and two radii */
-	class Torus : public Shape
-	{
+	class Torus : public Shape {
 		/** The OpenGL vertex buffer identifier */
 		GLuint _vertexBufferID = 0;
 
@@ -19,11 +18,11 @@ namespace Ray
 		int _materialIndex;
 
 		/** The material associated with the torus */
-		const class Material *_material;
+		const class Material* _material;
 
 	public:
 		/** This static method returns the directive describing the shape. */
-		static std::string Directive( void ){ return "shape_torus"; }
+		static std::string Directive(void) { return "shape_torus"; }
 
 		/** The center of the torus */
 		Util::Point3D center;
@@ -34,25 +33,29 @@ namespace Ray
 		/** The outer radius of the torus */
 		double oRadius;
 
+		/** The mesh of the cone */
+		std::vector<Triangle> mesh;
+
 		/** The default constructor */
-		Torus( void );
+		Torus(void);
 
 		///////////////////
 		// Shape methods //
 		///////////////////
 	private:
-		void _write( std::ostream &stream ) const;
-		void _read( std::istream &stream );
+		void _write(std::ostream& stream) const override;
+		void _read(std::istream& stream) override;
 	public:
-		std::string name( void ) const { return "torus"; }
-		void init( const class LocalSceneData &data );
-		void initOpenGL( void );
-		void updateBoundingBox( void );
-		double intersect( Util::Ray3D ray , class RayShapeIntersectionInfo &iInfo , Util::BoundingBox1D range = Util::BoundingBox1D( Util::Epsilon , Util::Infinity ) , std::function< bool (double) > validityFunction = [] ( double t ){ return true; } ) const;
-		bool isInside( Util::Point3D p ) const;
-		void drawOpenGL( GLSLProgram * glslProgram ) const;
-		size_t primitiveNum( void ) const;
+		std::string name(void) const override { return "torus"; }
+		void init(const class LocalSceneData& data) override;
+		void initOpenGL(void) override;
+		void updateBoundingBox(void) override;
+		double intersect(Util::Ray3D ray, class RayShapeIntersectionInfo& iInfo,
+		                 Util::BoundingBox1D range = Util::BoundingBox1D(Util::Epsilon, Util::Infinity),
+		                 std::function<bool (double)> validityFunction = [](double t) { return true; }) const override;
+		bool isInside(Util::Point3D p) const override;
+		void drawOpenGL(GLSLProgram* glslProgram) const override;
+		size_t primitiveNum(void) const override;
 	};
 }
 #endif // TORUS_INCLUDED
-
--- Ray/torus.todo.cpp
+++ Ray/torus.todo.cpp
@@ -36,8 +36,50 @@ void Torus::initOpenGL(void) {
 	///////////////////////////
 	// Do OpenGL set-up here //
 	///////////////////////////
-	WARN_ONCE("method undefined");
-
+	// implemented from https://gamedev.stackexchange.com/questions/16845/how-do-i-generate-a-torus-mesh
+	// the outer loop iterates through the major radius of the torus and finds vertical slices to triangulate
+	// the inner loop then subdivides the vertical slice into horizontal sectors, which results in a quad
+	// v1--v3
+	// |  / |
+	// | /  |
+	// v2--v4
+	const int complexity = OpenGLTessellationComplexity;
+	for (int i = 0; i < 2 * complexity; i++) {
+		for (int j = 0; j < 2 * complexity; j++) {
+			const double sin_theta = sin(Pi / complexity * i);
+			const double cos_theta = cos(Pi / complexity * i);
+			const double sin_theta_step = sin(Pi / complexity * (i + 1));
+			const double cos_theta_step = cos(Pi / complexity * (i + 1));
+			const double sin_phi = sin(Pi / complexity * j);
+			const double cos_phi = cos(Pi / complexity * j);
+			const double sin_phi_step = sin(Pi / complexity * (j + 1));
+			const double cos_phi_step = cos(Pi / complexity * (j + 1));
+			const double d = (oRadius + iRadius * sin_phi);
+			const double d_step = (oRadius + iRadius * sin_phi_step);
+			const auto v1 = new Vertex();
+			const auto v2 = new Vertex();
+			const auto v3 = new Vertex();
+			const auto v4 = new Vertex();
+			v1->position = center + Point3D(d * cos_theta, iRadius * cos_phi, d * sin_theta);
+			v2->position = center + Point3D(d * cos_theta_step, iRadius * cos_phi, d * sin_theta_step);
+			v3->position = center + Point3D(d_step * cos_theta, iRadius * cos_phi_step, d_step * sin_theta);
+			v4->position = center + Point3D(d_step * cos_theta_step, iRadius * cos_phi_step, d_step * sin_theta_step);
+			v1->normal = Point3D(sin_phi * cos_theta, cos_phi, sin_phi * sin_theta);
+			v2->normal = Point3D(sin_phi * cos_theta_step, cos_phi, sin_phi * sin_theta_step);
+			v3->normal = Point3D(cos_theta * sin_phi_step, cos_phi_step, sin_theta * sin_phi_step);
+			v4->normal = Point3D(cos_theta_step * sin_phi_step, cos_phi_step, sin_theta_step * sin_phi_step);
+			v1->texCoordinate = Point2D(static_cast<double>(i) / (2 * complexity),
+			                            static_cast<double>(j) / (2 * complexity));
+			v2->texCoordinate = Point2D(static_cast<double>(i + 1) / (2 * complexity),
+			                            static_cast<double>(j) / (2 * complexity));
+			v3->texCoordinate = Point2D(static_cast<double>(i) / (2 * complexity),
+			                            static_cast<double>(j + 1) / (2 * complexity));
+			v4->texCoordinate = Point2D(static_cast<double>(i + 1) / (2 * complexity),
+			                            static_cast<double>(j + 1) / (2 * complexity));
+			mesh.emplace_back(v1, v2, v3);
+			mesh.emplace_back(v2, v4, v3);
+		}
+	}
 	// Sanity check to make sure that OpenGL state is good
 	ASSERT_OPEN_GL_STATE();
 }
@@ -65,8 +107,8 @@ void Torus::drawOpenGL(GLSLProgram* glslProgram) const {
 	//////////////////////////////
 	// Do OpenGL rendering here //
 	//////////////////////////////
-	THROW("method undefined");
-
+	_material->drawOpenGL(glslProgram);
+	for (const auto& triangle : mesh) triangle.drawOpenGL(glslProgram);
 	// Sanity check to make sure that OpenGL state is good
 	ASSERT_OPEN_GL_STATE();
 }
