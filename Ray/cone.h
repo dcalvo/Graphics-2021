e2ecf8d942aa0335a323e1907bb1e3a92032cb4f
David <dcalvo2@jhu.edu>
2021-11-16 20:04:35 -0500

    implement cone

--- Ray/cone.h
+++ Ray/cone.h
@@ -3,14 +3,13 @@
 #include <Util/geometry.h>
 #include <Util/polynomial.h>
 #include "shape.h"
+#include "triangle.h"
 
-namespace Ray
-{
+namespace Ray {
 	/** This class represents a cone whose central axis is parallel to the y-axis, and 
 	* is defined by the center of the cone, the height from the tip to the base
 	* and the base of the cone. */
-	class Cone : public Shape
-	{
+	class Cone : public Shape {
 		/** The OpenGL vertex buffer identifier */
 		GLuint _vertexBufferID = 0;
 
@@ -21,7 +20,7 @@ namespace Ray
 		int _materialIndex;
 
 		/** The material associated with the cone */
-		const class Material *_material;
+		const class Material* _material;
 	public:
 		/** The center of the cone */
 		Util::Point3D center;
@@ -32,27 +31,32 @@ namespace Ray
 		/** The radius of the cone */
 		double radius;
 
+		/** The mesh of the cone */
+		std::vector<Triangle> mesh;
+
 		/** This static method returns the directive describing the shape. */
-		static std::string Directive( void ){ return "shape_cone"; }
+		static std::string Directive(void) { return "shape_cone"; }
 
 		/** The default constructor */
-		Cone( void );
+		Cone(void);
 
 		///////////////////
 		// Shape methods //
 		///////////////////
 	private:
-		void _write( std::ostream &stream ) const;
-		void _read( std::istream &stream );
+		void _write(std::ostream& stream) const override;
+		void _read(std::istream& stream) override;
 	public:
-		std::string name( void ) const { return "cone"; }
-		void init( const class LocalSceneData &data );
-		void initOpenGL( void );
-		void updateBoundingBox( void );
-		double intersect( Util::Ray3D ray , class RayShapeIntersectionInfo &iInfo , Util::BoundingBox1D range = Util::BoundingBox1D( Util::Epsilon , Util::Infinity ) , std::function< bool (double) > validityFunction = [] ( double t ){ return true; } ) const;
-		bool isInside( Util::Point3D p ) const;
-		void drawOpenGL( GLSLProgram *glslProgram ) const;
-		size_t primitiveNum( void ) const;
+		std::string name(void) const override { return "cone"; }
+		void init(const class LocalSceneData& data) override;
+		void initOpenGL(void) override;
+		void updateBoundingBox(void) override;
+		double intersect(Util::Ray3D ray, class RayShapeIntersectionInfo& iInfo,
+		                 Util::BoundingBox1D range = Util::BoundingBox1D(Util::Epsilon, Util::Infinity),
+		                 std::function<bool (double)> validityFunction = [](double t) { return true; }) const override;
+		bool isInside(Util::Point3D p) const override;
+		void drawOpenGL(GLSLProgram* glslProgram) const override;
+		size_t primitiveNum(void) const override;
 	};
 }
 #endif // CONE_INCLUDED
--- Ray/cone.todo.cpp
+++ Ray/cone.todo.cpp
@@ -37,8 +37,49 @@ void Cone::initOpenGL(void) {
 	/////////////////////////////////////////
 	// Do any necessary OpenGL set-up here //
 	/////////////////////////////////////////
-	WARN_ONCE("method undefined");
-
+	// generate 2 triangles each loop
+	// 1 from the center to the edges at theta and phi
+	// v1---v2 
+	//  \   / 
+	//   \ / 
+	//   bot
+	// 1 for the triangle enclosed by theta, phi, and the tip
+	//    top
+	//    / |
+	//   /  |
+	// v1--v2
+	const int complexity = OpenGLTessellationComplexity;
+	const auto bot = new Vertex();
+	bot->position = center;
+	bot->normal = Point3D(0., -1., 0.);
+	for (int i = 0; i < 2 * complexity; i++) {
+		const double sin_theta = sin(Pi / complexity * i);
+		const double cos_theta = cos(Pi / complexity * i);
+		const double sin_phi = sin(Pi / complexity * (i + 1));
+		const double cos_phi = cos(Pi / complexity * (i + 1));
+		const auto v1 = new Vertex();
+		const auto v2 = new Vertex();
+		v1->position = bot->position + Point3D(radius * cos_theta, 0, radius * sin_theta);
+		v2->position = bot->position + Point3D(radius * cos_phi, 0, radius * sin_phi);
+		v1->normal = v2->normal = Point3D(0., -1., 0.);
+		mesh.emplace_back(v2, bot, v1);
+		const auto v3 = new Vertex();
+		const auto v4 = new Vertex();
+		v3->position = v1->position;
+		v4->position = v2->position;
+		v3->normal = (v3->position - bot->position).unit();
+		v3->normal = Point3D(v3->normal[0] * height / radius, radius / height, v3->normal[2] * height / radius).unit();
+		v4->normal = (v4->position - bot->position).unit();
+		v4->normal = Point3D(v4->normal[0] * height / radius, radius / height, v4->normal[2] * height / radius).unit();
+		v3->texCoordinate = Point2D(static_cast<double>(i) / (2 * complexity), 0);
+		v4->texCoordinate = Point2D(static_cast<double>(i + 1) / (2 * complexity), 0);
+		// each triangle gets a new top so we can wrap a texture properly
+		const auto top = new Vertex();
+		top->position = center + Point3D(0., height, 0.);
+		top->normal = Point3D(0., 1., 0.);
+		top->texCoordinate = Point2D(static_cast<double>(i) / (2 * complexity), 1);
+		mesh.emplace_back(v4, v3, top);
+	}
 	// Sanity check to make sure that OpenGL state is good
 	ASSERT_OPEN_GL_STATE();
 }
@@ -66,8 +107,8 @@ void Cone::drawOpenGL(GLSLProgram* glslProgram) const {
 	//////////////////////////////
 	// Do OpenGL rendering here //
 	//////////////////////////////
-	THROW("method undefined");
-
+	_material->drawOpenGL(glslProgram);
+	for (const auto& triangle : mesh) triangle.drawOpenGL(glslProgram);
 	// Sanity check to make sure that OpenGL state is good
 	ASSERT_OPEN_GL_STATE();
 }
